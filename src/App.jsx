83935c703aea9b89eb907cb0acc4db0687b0df7a
Prabhat <Prabhat78961@gmail.com>
2026-06-26 21:55:45 +0530

    Nav done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, { useRef } from 'react'
 import CustomCursor from './Components/CustomCursor'
+import Navbar from './Components/Navbar'
 
 const App = () => {
   return (
     <div>
       <CustomCursor />
+      <Navbar />
     </div>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-        <span></span>
-    </nav>
+    <div className='fixed top-0 w-full z-50 bg-background\/80 backdrop-blur-md pt-1.5 pb-1.5 border-b border-hairline-border flex justify-between items-center max-w-full pl-10 pr-10'>
+        <div className="font-sans text-[32px] leading-[-0.05em] font-semibold text-acid ">PB</div>
+        <div className='hidden justify-between w-100 md:flex text-[13px] leading-[1.2] tracking-widest font-medium font-[FiraCode]'>
+            <a href="#Projects" className='text-[#CAC2BD] hover:text-[#FFFFFF] hover:transition-colors duration-300 '>Projects</a>
+            <a href="#Skills" className='text-[#CAC2BD] hover:text-[#FFFFFF] hover:transition-colors duration-300'>Skills</a>
+            <a href="#Experiece" className='text-[#CAC2BD] hover:text-[#FFFFFF] hover:transition-colors duration-300'>Experience</a>
+            <a href="#Contact" className='text-[#CAC2BD] hover:text-[#FFFFFF] hover:transition-colors duration-300'>Contact</a>
+        </div>
+            <button className='rounded-xs bg-white pl-7 pr-7 text-[#31302B] flex-wrap font-[FiraCode] pt-1.5 pb-1.5 duration-200 transition-transform active:scale-[0.95]'>
+              Hire Me
+            </button>
+    </div>
   )
 }
 
